00d662c69d99c9c8954f4574d1b55da73222e684
Sijaja <m.sijaja@gmx.at>
2025-09-16 09:13:58 +0200

    First commit

---
+++ src/main/java/dev/sijaja/serviceheft/ServiceheftApplication.java
@@ -0,0 +1,13 @@
+package dev.sijaja.serviceheft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceheftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceheftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/sijaja/serviceheft/ServiceheftApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sijaja.serviceheft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceheftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
